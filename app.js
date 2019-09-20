78a81bb4503341a7d5a19eb872c1db41629c1873
root <root@ip-10-0-1-143.cn-northwest-1.compute.internal>
2019-09-20 00:01:43 +0000

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...")
 var handler = function(req, res) {
     console.log("Received request from " + req.connection.remoteAddress)
     res.writeHead(200)
-    res.end("You've attach " + os.hostname() + "\n")
+    res.end("You've hit " + os.hostname() + "\n")
 }
 
 var www = http.createServer(handler)
